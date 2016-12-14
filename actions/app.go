67e85178e19f82ee4d8581f66e44ce514a49bd92
Brian Ketelsen <bketelsen@gmail.com>
2016-12-14 11:57:16 -0500

    log environment at start

--- actions/app.go
+++ actions/app.go
@@ -2,20 +2,24 @@ package actions
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gopheracademy/gcon/actions/admin"
 	"github.com/gopheracademy/gcon/models"
 	"github.com/markbates/buffalo"
 	"github.com/markbates/buffalo/middleware"
+	"github.com/markbates/going/defaults"
 )
 
 // App is where all routes and middleware for buffalo
 // should be defined. This is the nerve center of your
 // application.
 func App() http.Handler {
+	env := defaults.String(os.Getenv("GO_ENV"), "development")
 	a := buffalo.Automatic(buffalo.Options{
-		Env: "development",
+		Env: env,
 	})
+	buffalo.Logger.Info("Environment", env)
 
 	a.Use(middleware.PopTransaction(models.DB))
 	a.ServeFiles("/assets", assetsPath())
