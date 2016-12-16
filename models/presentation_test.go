ad6e390b1479f5b690db03fc839e9371efa38bad
Charlie Griefer <charlie@Charlie-WBSL-MPB.local>
2016-12-16 10:12:07 -0700

    Issue-12: Created presentation model

---
+++ models/presentation_test.go
@@ -0,0 +1,18 @@
+package models_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/gopheracademy/gcon/models"
+)
+
+// Test_Presentation
+func Test_Presentation(t *testing.T) {
+	p := &models.Presentation{}
+	p.Title = "Go-ing The Distance"
+	p.ShortDescription = "Reading Charlie's Blog"
+	if m := p.String(); !strings.Contains(m, "Distance") {
+		t.Errorf("expected contains %s, got %s", "Distance", m)
+	}
+}
