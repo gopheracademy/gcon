e606ce5d3b5faf990f09c6a9b7095d6631d33820
Brian Ketelsen <bketelsen@gmail.com>
2016-12-19 20:33:17 +0000

    better stack overflow answer :)

--- deployment/gce.sh
+++ deployment/gce.sh
@@ -10,14 +10,6 @@ docker tag $IMAGE:$COMMIT $IMAGE:latest
 docker tag $IMAGE:$COMMIT $IMAGE:travis-$TRAVIS_BUILD_NUMBER
 docker push $IMAGE
 
-gcloud version
-gcloud components update kubectl || true
-curl https://sdk.cloud.google.com | bash
-bash -l ./gcloud.sh version
-bash -l ./gcloud.sh components update kubectl
-
-#curl https://sdk.cloud.google.com | bash
-#source /home/travis/.bashrc
-#gcloud components update kubectl
+gcloud components update kubectl
 kubectl get pods
 kubectl patch deployment $DEPLOYMENT -p '{"spec":{"template":{"spec":{"containers":[{"name":"'"$CONTAINER_NAME"'","image":"'"$IMAGE_VERSION"'"}]}}}}'
