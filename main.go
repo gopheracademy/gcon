6d65590e39c7271f3ae94890d1dea7b7ad24de07
Brian Ketelsen <bketelsen@gmail.com>
2017-01-06 10:26:10 -0500

    binary name

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ import (
 
 func main() {
 	port := defaults.String(os.Getenv("PORT"), "3000")
-	log.Printf("Starting temp on port %s\n", port)
+	log.Printf("Starting gcon on port %s\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), actions.App()))
 }
