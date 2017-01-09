92476b13a9b2ed2c009a2a04c9b2646f9338b105
Brian Downs <brian.downs@gmail.com>
2017-01-09 07:18:06 -0700

    issue-60: added on_sale field to hotels model and generated new migrations

--- models/hotel.go
+++ models/hotel.go
@@ -11,10 +11,11 @@ type Hotel struct {
 	CreatedAt  time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at" db:"updated_at"`
 	BlockCode  string    `json:"block_code" db:"block_code"`
-	RoomRate   int       `json:"room_rate" db:"room_rate"`
+	RoomRate   float64   `json:"room_rate" db:"room_rate"`
 	PhotoURL   string    `json:"photo_url" db:"photo_url"`
 	LocationID int       `json:"location_id" db:"location_id"`
 	ContactID  int       `json:"contact_id" db:"contact_id"`
+	OnSale     bool      `json:"on_sale" db:"on_sale"`
 }
 
 // String is not required by pop and may be deleted
--- models/hotel_test.go
+++ models/hotel_test.go
@@ -7,16 +7,6 @@ import (
 	"github.com/gopheracademy/gcon/models"
 )
 
-// Test_Hotel
-func Test_Hotel(t *testing.T) {
-	h := &models.Hotel{}
-	h.BlockCode = "GOPHERCON2017"
-	h.RoomRate = 225
-	if m := h.String(); !strings.Contains(m, "GOPHERCON2017") {
-		t.Errorf("expected contains %s, got %s", "GOPHERCON2017", m)
-	}
-}
-
 // Test_Hotels
 func Test_Hotels(t *testing.T) {
 	h := &models.Hotels{
